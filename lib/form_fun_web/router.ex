0aa693996d70053033c2d04bd6c61e09a9f74172
Ben Wilson <benwilson512@gmail.com>
2023-04-13 16:04:49 -0400

    users form

--- lib/form_fun_web/router.ex
+++ lib/form_fun_web/router.ex
@@ -17,6 +17,13 @@ defmodule FormFunWeb.Router do
   scope "/", FormFunWeb do
     pipe_through :browser
 
+    live "/users", UserLive.Index, :index
+    live "/users/new", UserLive.Index, :new
+    live "/users/:id/edit", UserLive.Index, :edit
+
+    live "/users/:id", UserLive.Show, :show
+    live "/users/:id/show/edit", UserLive.Show, :edit
+
     get "/", PageController, :home
   end
 
